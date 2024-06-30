63530af931b74e6e941757f885f494422badb070
Hasan Chowdhury <chowdhury.hms@gmail.com>
2024-07-01 00:02:43 +0600

    Setup the project boilerplate

---
+++ app/endpoints/v1/class-settings.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Google Auth Shortcode.
+ *
+ * @link          https://themedyno.com/
+ * @since         1.0.0
+ *
+ * @author        AFFIMPORTR (https://themedyno.com)
+ * @package       AFFIMPORTR\PluginTest
+ *
+ * @copyright (c) 2024, ThemeDyno (http://themedyno.com)
+ */
+
+namespace AFFIMPORTR\App\Endpoints\V1;
+
+// Abort if called directly.
+defined( 'WPINC' ) || die;
+
+use AFFIMPORTR\Core\Endpoint;
+
+class Settings extends Endpoint {
+	/**
+	 * API endpoint for the current endpoint.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string $endpoint
+	 */
+	protected $endpoint = 'settings';
+
+	/**
+	 * Register the routes for handling auth functionality.
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 *
+	 */
+	public function register_routes() {
+		// TODO
+		// Add a new Route to logout.
+
+		// Route to get auth url.
+		register_rest_route(
+			$this->get_namespace(),
+			$this->get_endpoint(),
+			array(
+				array(
+					'methods'             => 'GET',
+					'callback'            => array(
+						$this,
+						'get_settings',
+					),
+					'permission_callback' => array(
+						$this,
+						'edit_permission',
+					),
+				),
+				array(
+					'methods'             => 'POST',
+					'args'                => array(
+						'data' => array(
+							'required'    => true,
+							'description' => __( 'The client ID from Google API project.', 'affiliate-product-importer' ),
+							'type'        => 'object',
+						),
+					),
+					'callback'            => array(
+						$this,
+						'save_settings',
+					),
+					'permission_callback' => array(
+						$this,
+						'edit_permission',
+					),
+				),
+			)
+		);
+	}
+
+	/**
+	 * Save the client id and secret.
+	 *
+	 *
+	 * @since 1.0.0
+	 */
+	public function get_settings( \WP_REST_Request $request ) {
+		$nonce = $request->get_header( 'X-WP-NONCE' );
+		if ( ! wp_verify_nonce( $nonce, 'wp_rest' ) ) {
+			return new WP_REST_Response( 'Invalid nonce', 403 );
+		}
+
+		$affimportr_pixel_data = get_option( 'affimportr_pixel_data' );
+
+		// Decode the JSON data
+		$decoded_data = ( $affimportr_pixel_data ) ? json_decode( $affimportr_pixel_data, true ) : null;
+
+		// Escape each item in the decoded data
+		if ( is_array( $decoded_data ) ) {
+			$escaped_data = array_map( 'esc_html', $decoded_data );
+		} else {
+			$escaped_data = null;
+		}
+
+		$response_data = array(
+			'affimportr_pixel_data' => $escaped_data,
+		);
+
+		return new \WP_REST_Response( $response_data, 200 );
+	}
+
+	/**
+	 * Save the client id and secret.
+	 *
+	 *
+	 * @since 1.0.0
+	 */
+	public function save_settings( \WP_REST_Request $request ) {
+		$nonce = $request->get_header( 'X-WP-NONCE' );
+		if ( ! wp_verify_nonce( $nonce, 'wp_rest' ) ) {
+			return new WP_REST_Response( 'Invalid nonce', 403 );
+		}
+
+		// Validate and sanitize the input data
+		$data           = isset( $request['data'] ) ? $request['data'] : array();
+		$sanitized_data = array_map( 'sanitize_text_field', $data ); // Sanitize each item in the data array
+
+		// Encode the sanitized data to JSON format
+		$affimportr_pixel_data = wp_json_encode( $sanitized_data );
+		update_option( 'affimportr_pixel_data', $affimportr_pixel_data );
+
+		return new \WP_REST_Response( array( 'message' => 'Settings saved' ), 200 );
+	}
+}
---
+++ core/class-singleton.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Singleton class for all classes.
+ *
+ * @link    https://themedyno.com/
+ * @since   1.0.0
+ *
+ * @author  AFFIMPORTR (https://themedyno.com)
+ * @package AFFIMPORTR_Core
+ *
+ * @copyright (c) 2024, ThemeDyno (http://themedyno.com)
+ */
+
+namespace AFFIMPORTR\Core;
+
+// Abort if called directly.
+defined( 'WPINC' ) || die;
+
+/**
+ * Class Singleton
+ *
+ * @package AFFIMPORTR\Core
+ */
+abstract class Singleton {
+
+	/**
+	 * Singleton constructor.
+	 *
+	 * Protect the class from being initiated multiple times.
+	 *
+	 * @param array $props Optional properties array.
+	 *
+	 * @since 1.0.0
+	 */
+	protected function __construct( $props = array() ) {
+		// Protect class from being initiated multiple times.
+	}
+
+	/**
+	 * Instance obtaining method.
+	 *
+	 * @return static Called class instance.
+	 * @since 1.0.0
+	 */
+	public static function instance() {
+		static $instances = array();
+
+		// @codingStandardsIgnoreLine Plugin-backported
+		$called_class_name = get_called_class();
+
+		if ( ! isset( $instances[ $called_class_name ] ) ) {
+			$instances[ $called_class_name ] = new $called_class_name();
+		}
+
+		return $instances[ $called_class_name ];
+	}
+}
