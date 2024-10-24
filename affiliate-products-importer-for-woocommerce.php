ae76f3f78d6f079b13238f56130940dc60448e97
Hasan Chowdhury <chowdhury.hms@gmail.com>
2024-10-25 00:37:10 +0600

    Release version 1.0.5

--- affiliate-products-importer-for-woocommerce.php
+++ affiliate-products-importer-for-woocommerce.php
@@ -7,7 +7,7 @@
  * Description:     Easily import Amazon affiliate products into your WooCommerce store.
  * Author:          ThemeDyno
  * Author URI:      https://themedyno.com/
- * Version:         1.0.4
+ * Version:         1.0.5
  * Text Domain:     affiliate-products-importer-for-woocommerce
  * Domain Path:     /languages
  *
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: Amazon Affiliate, Products Importer for WooCommerce
 Requires at least: 5.2
 Tested up to: 6.6.2
 Requires PHP: 7.2.0
-Stable tag: 1.0.4
+Stable tag: 1.0.5
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.en.html
 
